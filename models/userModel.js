eb67fc4ec0b1b685eb65e8361e7de7e98581bc3d
Jorge David Terrero <jo.terrero@dga.gov.do>
2020-02-21 14:56:19 -0400

    adding user model to the proyect

---
+++ models/userModel.js
@@ -0,0 +1,25 @@
+const mongoose = require('mongoose');
+const {Schema} = mongoose;
+
+const userSchema = new Schema({
+
+    username: {
+        type: String,
+        required: true
+    } ,
+   email: {
+       type: String,
+       required: true
+   },
+   password:{
+       type: String,
+       required: true
+   },
+   status:{
+       typed: Boolean,
+       default: true
+   }
+
+});
+
+module.exports = mongoose.model('Users' , userSchema);
