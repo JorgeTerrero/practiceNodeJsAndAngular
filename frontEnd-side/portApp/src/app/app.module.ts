b203769a527255d3554f54157afbbec77de1c7ab
Jorge David Terrero <jo.terrero@dga.gov.do>
2020-02-21 16:17:35 -0400

    adding components

--- frontEnd-side/portApp/src/app/app.module.ts
+++ frontEnd-side/portApp/src/app/app.module.ts
@@ -2,10 +2,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/components/login/login.component';
+import { HeadersComponent } from './components/components/headers/headers.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HeadersComponent
   ],
   imports: [
     BrowserModule
