d5a922f68de234c6a323d48590e2b9f10cc55c12
Jorge David Terrero <jo.terrero@dga.gov.do>
2020-02-21 17:00:47 -0400

    adding cors

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,9 +1,10 @@
 const controller = require('../controllers/userControllers');
 const express = require('express');
+const cors = require('cors');
 const {Router} = express;
 const router = Router();
 
-router.post('/api/register' , controller.regiterUser);
-router.post('/api/login' , controller.loginUser);
+router.post('/api/register' , cors(), controller.regiterUser);
+router.post('/api/login' , cors(),controller.loginUser);
 
 module.exports = router;
