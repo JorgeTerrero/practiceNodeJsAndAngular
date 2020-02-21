2cb72c75de4fd4b64c8b65ac04fee4bcca510aec
Jorge David Terrero <jo.terrero@dga.gov.do>
2020-02-21 15:09:42 -0400

    adding user controlle to proyect

---
+++ controllers/userControllers.js
@@ -0,0 +1,37 @@
+const userModel = require('../models/userModel');
+const bcrypt = require('bcrypt');
+
+const userController = {};
+
+userController.regiterUser = async (req, res) => {
+
+    try {
+
+        let body = req.body;
+        let passwordSalt = 10;
+
+        bcrypt.hash(body.password, passwordSalt, async function (err, hash) {
+
+            let _user = new userModel({
+                username: body.name,
+                email: body.email,
+                password: hash
+            });
+
+            await _user.save();
+
+            res.json({
+                Ok: true,
+                message: 'User was Registred Correctly'
+            });
+
+        });
+
+    } catch (err) {
+        res.status(400).json({
+            Ok: false,
+            err
+        });
+    }
+
+};
